14e621325fb2fc6042695c88c102d5448fd1491b
Nadir Ouzlim <ouzlimnadir@gmail.com>
2023-03-21 18:50:57 +0000

    Initial commit

---
+++ src/main/java/com/fsa/ProLog/ProLogApplication.java
@@ -0,0 +1,13 @@
+package com.fsa.ProLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsa/ProLog/ProLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsa.ProLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
